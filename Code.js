59bda7f121283bf3498241e6b4594383e9a3d9c7
AB-TechGen <68916932+AB-TechGen@users.noreply.github.com>
2021-07-07 17:16:57 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("U N C F I O T N", 700,150)
+    fill("lightblue")
+    text("Hint: Something which performs a particular task", 700, 170)
+
+    fill("white")
+    textSize(15)
+    text("ATADASBA", 100,250)
+    fill("lightblue")
+    text("Hint: Something which stores information", 200, 270)
+
+
+
+}
